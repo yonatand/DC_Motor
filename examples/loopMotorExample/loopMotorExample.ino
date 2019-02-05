23d6fc0c09e1c0ab01143a1e3a1e746300907ed1
yonatand <yonadolg@gmail.com>
2019-02-05 20:08:01 +0200

    uploaded the library with 2 examples.

---
+++ examples/loopMotorExample/loopMotorExample.ino
@@ -0,0 +1,39 @@
+#include <DC_Motor.h>
+
+//create the DC_Motor object
+DC_Motor motor = DC_Motor(0,1,2,3);
+
+void setup() {
+  //set the speed of the object (quarter speed)
+  motor.speedMotor(64);
+}
+
+void loop() {
+  //set direction
+  motor.modeMotor(0,1);
+  //submit settings
+  motor.submitMotor();
+  //wait 2 seconds
+  delay(2000);
+
+  //stop the motor
+  motor.modeMotor(0,0);
+  //submit settings
+  motor.submitMotor();
+  //wait 2 seconds
+  delay(2000);
+
+  //set direction
+  motor.modeMotor(1,0);
+  //submit settings
+  motor.submitMotor();
+  //wait 2 seconds
+  delay(2000);
+
+  //stop the motor
+  motor.modeMotor(0,0);
+  //submit settings
+  motor.submitMotor();
+  //wait 2 seconds
+  delay(2000);
+}
